6231ff0469cb72609fd991b1d58638e988caa647
Jinsuk <totorovirus@gmail.com>
2023-06-03 18:47:52 +0900

    finish relayer server

--- services/relayer_server.py
+++ services/relayer_server.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import json
 import os
 import tempfile
@@ -8,15 +9,19 @@ import httpx
 import ezkl_lib
 import numpy as np
 import torch
-from web3 import AsyncHTTPProvider, AsyncWeb3
+from web3 import HTTPProvider, Web3
+from web3.middleware import construct_sign_and_send_raw_middleware
 import rlp
 import eth_utils
+import eth_account
 
 from models import SimpleModel
 
 
 INFURA_URL='https://mainnet.infura.io/v3/5c92864a308b45b6a8c3559b63cb5b38'
 THEGRAPH_URL=''
+PRIVATE_KEY = os.getenv('PRIVATE_KEY')
+ACCOUNT = eth_account.Account.from_key(PRIVATE_KEY)
 
 
 class RpcRequest(BaseModel):
@@ -66,23 +71,76 @@ async def get_features(address):
     return np.random.randn(16)
 
 
-async def check_fraud_address(address):
-    input_data = get_features(address)
-    input_data = torch.tensor(input_data, dtype=torch.float32)
-    output = model(input_data).numpy()
+async def check_fraud(input_data):
+    with torch.no_grad():
+        input_data = torch.tensor(input_data, dtype=torch.float32)
+        output = model(input_data).numpy()
     score = output[0]
     proof = generate_proof(input_data, output)
     return score, proof
+
+
+async def get_contract_wallet_address(eoa_address):
+    """Get contract wallet address from EOA wallet address using the graph
+    protocol."""
+    return '0x000000000000000000000000000000000000dEaD'
+
+
+w3 = Web3(HTTPProvider(INFURA_URL))
+account = w3.eth.account.from_key(PRIVATE_KEY)
+w3.middleware_onion.add(construct_sign_and_send_raw_middleware(account))
+contract_abi = json.load(open('./Wallet.json', 'r'))
+
+
+@dataclass
+class Transaction:
+    nonce: int
+    gasPrice: int
+    gasLimit: int
+    to: str
+    value: int
+    data: bytes
+    from_: str
+    r: bytes
+    v: int
+    s: bytes
+
+    @classmethod
+    def from_abi_encoded(cls, data):
+        data = rlp.decode(data)
+        return cls(nonce=eth_utils.to_int(data[0]),
+            gasPrice=eth_utils.to_int(data[1]),
+            gasLimit=eth_utils.to_int(data[2]),
+            to=eth_utils.to_hex(data[3]),
+            value=eth_utils.to_int(data[4]),
+            data=eth_utils.to_bytes(data[5]),
+            from_=eth_utils.to_hex(data[6]),
+            r=eth_utils.tobytes(data[7]),
+            v=eth_utils.to_int(data[8]),
+            s=eth_utils.tobytes(data[9]))
+
+    def serialize(self):
+        return [
+            self.nonce,
+            self.gasPrice,
+            self.gasLimit,
+            self.to,
+            self.value,
+            self.data,
+            self.from_,
+            self.r,
+            self.v,
+            self.s]
+
 
 
 @app.get("/")
 async def root(rpc: RpcRequest):
     if rpc.method == "eth_sendRawTransaction":
         if rpc.params:
-            data = rpc.params[0]
-            data = rlp.decode(data)
-            address = eth_utils.to_hex(data[3])
-            score, proof = await check_fraud_address(address)
+            transaction = Transaction.from_abi_encoded(rpc.params[0])
+            input_data = await get_features(transaction.to)
+            score, proof = await check_fraud(input_data)
             if score > 0.5:
                 return {
                     'jsonrpc': '2.0',
@@ -97,14 +155,26 @@ async def root(rpc: RpcRequest):
                     'id': rpc.id
                 }
             else:
-                w3 = AsyncWeb3(AsyncHTTPProvider(INFURA_URL))
-                # TODO: Send transaction to the pre-defined contract.
+                contract_address = await get_contract_wallet_address(
+                    transaction.from_)
+                wallet_contract = w3.eth.contract(
+                    address=contract_address, abi=contract_abi)
+                tx = wallet_contract.functions.execute(
+                    transaction.serialize(), input_data, proof).build_transaction(
+                        {
+                            'nonce': transaction.nonce,
+                            'from': account.address,
+                            'to': contract_address
+                        }
+                    )
+                tx_hash = w3.eth.send_transaction(tx)
+                return {
+                    'jsonrpc': '2.0',
+                    'resut': tx_hash,
+                    'id': rpc.id
+                }
 
-                result = await w3.eth.send_transaction(proof)
-                return result
-        else:
-            print('Error in message')
-
+    print('Relaying rpc message %s to infura' % str(rpc))
     async with httpx.AsyncClient() as client:
         response = await client.post(
             INFURA_URL,
