aceaaf5ada1bbe0cb3ee2e43af0541a430f05716
Jinsuk <totorovirus@gmail.com>
2023-06-04 02:19:40 +0900

    fix zero div error

--- services/transaction_features.py
+++ services/transaction_features.py
@@ -7,13 +7,14 @@ class EtherScan:
     UNIX_TIME_DENOMINATOR = 60
     SCALE = 1e+18
 
-    def __init__(self, address):
+    def __init__(self, address, base_url='https://api.etherscan.io/api'):
         self.address = address
+        self.base_url = base_url
 
     async def _set_normal_transaction_features(self):
         async with httpx.AsyncClient() as client:
             result  = await client.get(
-                'https://api.etherscan.io/api' +
+                self.base_url +
                 '?module=account' +
                 '&action=txlist' +
                 '&address=' + self.address +
@@ -23,7 +24,7 @@ class EtherScan:
                 '&apikey=' + self.API_TOKEN)
             self.__normal_transactions_json = result.json()
             result = await client.get(
-                'https://api.etherscan.io/api' +
+                self.base_url +
                 '?module=account'
                 '&action=balance'
                 '&address=' + self.address +
@@ -32,16 +33,16 @@ class EtherScan:
             self.__balance_json = result.json()
         sent_tnxs = [obj for obj in self.__normal_transactions_json["result"] if obj.get("from") == self.address]
         received_tnxs = [obj for obj in self.__normal_transactions_json["result"] if obj.get("to") == self.address]
-        contract_tnxs = [obj for obj in self.__normal_transactions_json["result"] if obj.get("to") == ""]
-        self.avg_min_between_sent_tnx = np.mean(np.diff([int(obj.get("timeStamp")) for obj in sent_tnxs])) / self.UNIX_TIME_DENOMINATOR
-        self.avg_min_between_received_tnx = np.mean(np.diff([int(obj.get("timeStamp")) for obj in received_tnxs])) / self.UNIX_TIME_DENOMINATOR
-        self.time_diff_between_first_and_last = (max(int(obj.get("timeStamp")) for obj in self.__normal_transactions_json["result"]) - min(int(obj.get("timeStamp")) for obj in self.__normal_transactions_json["result"])) / self.UNIX_TIME_DENOMINATOR
         self.sent_tnx = len(sent_tnxs)
         self.received_tnx = len(received_tnxs)
+        contract_tnxs = [obj for obj in self.__normal_transactions_json["result"] if obj.get("to") == ""]
+        self.avg_min_between_sent_tnx = np.mean(np.diff([int(obj.get("timeStamp")) for obj in sent_tnxs])) / self.UNIX_TIME_DENOMINATOR if self.sent_tnx != 0 else 0
+        self.avg_min_between_received_tnx = np.mean(np.diff([int(obj.get("timeStamp")) for obj in received_tnxs])) / self.UNIX_TIME_DENOMINATOR if self.sent_tnx != 0 else 0
+        self.time_diff_between_first_and_last = (max(int(obj.get("timeStamp")) for obj in self.__normal_transactions_json["result"]) - min(int(obj.get("timeStamp")) for obj in self.__normal_transactions_json["result"])) / self.UNIX_TIME_DENOMINATOR
         self.number_of_created_contracts = len([obj for obj in self.__normal_transactions_json["result"] if obj.get("to") == ""])
         self.max_value_received = max([int(obj.get("value")) for obj in received_tnxs]) / self.SCALE if len(received_tnxs) != 0 else 0
-        self.avg_val_received = np.average([int(obj.get("value")) for obj in received_tnxs]) / self.SCALE
-        self.avg_val_sent = np.average([int(obj.get("value")) for obj in sent_tnxs]) / self.SCALE
+        self.avg_val_received = np.average([int(obj.get("value")) for obj in received_tnxs]) / self.SCALE if self.received_tnx != 0 else 0
+        self.avg_val_sent = np.average([int(obj.get("value")) for obj in sent_tnxs]) / self.SCALE if self.sent_tnx != 0 else 0
         self.min_value_sent_to_contract = min([int(obj.get("value")) for obj in contract_tnxs]) / self.SCALE if len(contract_tnxs) != 0 else 0
         self.total_ether_sent = sum([int(obj.get("value")) for obj in sent_tnxs]) / self.SCALE
         self.total_ether_balance = int(self.__balance_json["result"]) / self.SCALE
@@ -49,14 +50,14 @@ class EtherScan:
     async def _set_erc_features(self):
         async with httpx.AsyncClient() as client:
             r = await client.get(
-            'https://api.etherscan.io/api' +
-            '?module=account' +
-            '&action=tokentx' +
-            '&address=' + self.address +
-            '&startblock=0' +
-            '&endblock=27025780' +
-            '&sort=asc' +
-            '&apikey=' + self.API_TOKEN)
+                self.base_url +
+                '?module=account' +
+                '&action=tokentx' +
+                '&address=' + self.address +
+                '&startblock=0' +
+                '&endblock=27025780' +
+                '&sort=asc' +
+                '&apikey=' + self.API_TOKEN)
 
         self.__erc20s_json = r.json()
 
