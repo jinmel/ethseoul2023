0823d5bb70da0b9feb44c4e9797ad16ba299c643
Jinsuk <totorovirus@gmail.com>
2023-06-04 02:17:43 +0900

    relayer server update

--- services/relayer_server.py
+++ services/relayer_server.py
@@ -1,28 +1,37 @@
 from dataclasses import dataclass
 import json
 import os
+from string import Template
 import tempfile
 
 from fastapi import FastAPI
 from pydantic import BaseModel
 import httpx
 import ezkl_lib
-import numpy as np
 import torch
 from web3 import HTTPProvider, Web3
 from web3.middleware import construct_sign_and_send_raw_middleware
 import rlp
 import eth_utils
 import eth_account
+from eth.vm.forks.arrow_glacier.transactions import ArrowGlacierTransactionBuilder as TransactionBuilder
 
 from models import SimpleModel
 from transaction_features import EtherScan
 
 
-INFURA_URL='https://mainnet.infura.io/v3/5c92864a308b45b6a8c3559b63cb5b38'
-THEGRAPH_URL=''
+INFURA_URL='https://goerli.infura.io/v3/5c92864a308b45b6a8c3559b63cb5b38'
+THEGRAPH_URL='https://api.thegraph.com/subgraphs/name/chee-chyuan/walletfactory'
 PRIVATE_KEY = os.getenv('PRIVATE_KEY')
 ACCOUNT = eth_account.Account.from_key(PRIVATE_KEY)
+WALLET_FACTORY_CONTRACT = '0xe652a8E383F55cB8D5c4fD2fD31849065231147F'
+
+
+w3 = Web3(HTTPProvider(INFURA_URL))
+account = w3.eth.account.from_key(PRIVATE_KEY)
+w3.middleware_onion.add(construct_sign_and_send_raw_middleware(account))
+contract_abi = json.load(open('./Wallet.json', 'r'))
+factory_contract_abi = json.load(open('./WalletFactory.json', 'r'))
 
 
 class RpcRequest(BaseModel):
@@ -44,31 +53,40 @@ model = load_model('./assets/model.pt')
 
 app = FastAPI()
 
+
+def ezkl_prove(input_filename, proof_filename):
+    cmd = (f'ezkl prove -D {input_filename} -M ./assets/model.onnx --pk-path ./assets/pk.key --proof-path {proof_filename} --params-path ./assets/kzg.params --transcript=evm --circuit-params-path ./assets/circuit.params')
+    print('Running %s' % cmd)
+    os.system(cmd)
+
+
 def generate_proof(input_data, output):
-    data = dict(input_shapes=[input_data.shape],
-                input_data=[input_data],
-                output_data=[((o).detach().numpy()).reshape([-1]).tolist()
-                             for o in output])
+    data = dict(input_shapes=[list(input_data.shape)],
+                input_data=[input_data.tolist()],
+                output_data=[o.reshape([-1]).tolist() for o in output])
     with tempfile.TemporaryDirectory() as tmpdirname:
         input_filename = os.path.join(tmpdirname, 'input.json')
+        proof_filename = os.path.join(tmpdirname, 'proof.enc')
+        print(f'Dumping input {data} to {input_filename}')
         with open(input_filename, 'w') as f:
             json.dump(data, f)
-        proof_filename = f'{tmpdirname}/proof.enc'
         print(f'Genearting proof to {proof_filename}')
-        ezkl_lib.prove(input_filename,
-                       './assets/model.onnx',
-                       './assets/pk.json',
-                       proof_filename,
-                       './assets/kzg.params',
-                       'evm', 'single',
-                       './assets/circuit.params')
+        ezkl_prove(input_filename, proof_filename)
+        # ezkl_lib.prove(input_filename,
+        #                './assets/model.onnx',
+        #                './assets/pk.key',
+        #                proof_filename,
+        #                './assets/kzg.params',
+        #                'evm',
+        #                'single',
+        #                './assets/circuit.params')
         print('Proof: %s' % ezkl_lib.print_proof_hex(proof_filename))
         with open(proof_filename, 'rb') as proof_file:
             return proof_file.read()
 
 
 async def get_features(address):
-    etherscan = EtherScan(address)
+    etherscan = EtherScan(address, base_url='https://api-goerli.etherscan.io/api')
     features = await etherscan.get_features_list()
     assert len(features) == 16, 'Expect 16 features'
     return features
@@ -78,26 +96,48 @@ async def check_fraud(input_data):
     with torch.no_grad():
         input_data = torch.tensor(input_data, dtype=torch.float32)
         output = model(input_data).cpu().numpy()
-    score = output[0]
     proof = generate_proof(input_data, output)
+    score = output[0]
     return score, proof
 
 
-MOCK_GRAPH = {
-    '0xAB51D1C24De70aCAC4162F29430a1C567420a9bd': '0x24A3103D0bE9C1a0416BE4824e0732c45f1C7774'
+query_template = Template("""
+query MyQuery {
+    deployedWallets(where: {owner: "$address"}) {
+        owner
+        transactionHash
+        walletAddress
+        blockTimestamp
+        id
+        blockNumber
+    }
 }
+""")
 
 
 async def get_contract_wallet_address(eoa_address):
     """Get contract wallet address from EOA wallet address using the graph
     protocol."""
-    return MOCK_GRAPH[eoa_address]
+    async with httpx.AsyncClient() as client:
+        response = await client.post(THEGRAPH_URL, json={
+            "query": query_template.substitute(address=eoa_address),
+            "variables":{}})
+    return response.json()['data']['deployedWallets'][0]['walletAddress']
 
 
-w3 = Web3(HTTPProvider(INFURA_URL))
-account = w3.eth.account.from_key(PRIVATE_KEY)
-w3.middleware_onion.add(construct_sign_and_send_raw_middleware(account))
-contract_abi = json.load(open('./Wallet.json', 'r'))
+class SerializedTransaction(rlp.Serializable):
+    fields = [
+        ('nonce', rlp.sedes.big_endian_int),
+        ('gasPrice', rlp.sedes.big_endian_int),
+        ('gasLimit', rlp.sedes.big_endian_int),
+        ('to', rlp.sedes.Text.fixed_length(20, allow_empty=True)),
+        ('value', rlp.sedes.big_endian_int),
+        ('data', rlp.sedes.Binary(allow_empty=True)),
+        ('from', rlp.sedes.Text.fixed_length(20, allow_empty=True)),
+        ('r', rlp.sedes.binary),
+        ('v', rlp.sedes.big_endian_int),
+        ('s', rlp.sedes.binary)
+    ]
 
 
 @dataclass
@@ -114,8 +154,11 @@ class Transaction:
     s: bytes
 
     @classmethod
-    def from_abi_encoded(cls, data):
-        data = rlp.decode(data)
+    def from_hexstring(cls, data):
+        data = eth_utils.to_bytes(hexstr=data)
+        print(data)
+        data = rlp.decode(data, SerializedTransaction)
+        print(data)
         return cls(nonce=eth_utils.to_int(data[0]),
             gasPrice=eth_utils.to_int(data[1]),
             gasLimit=eth_utils.to_int(data[2]),
@@ -123,9 +166,9 @@ class Transaction:
             value=eth_utils.to_int(data[4]),
             data=eth_utils.to_bytes(data[5]),
             from_=eth_utils.to_hex(data[6]),
-            r=eth_utils.tobytes(data[7]),
+            r=eth_utils.to_bytes(data[7]),
             v=eth_utils.to_int(data[8]),
-            s=eth_utils.tobytes(data[9]))
+            s=eth_utils.to_bytes(data[9]))
 
     def serialize(self):
         return [
@@ -138,15 +181,19 @@ class Transaction:
             self.from_,
             self.r,
             self.v,
-            self.s]
+            self.s
+        ]
 
 
-@app.get("/")
+@app.post("/")
 async def root(rpc: RpcRequest):
     if rpc.method == "eth_sendRawTransaction":
         if rpc.params:
-            transaction = Transaction.from_abi_encoded(rpc.params[0])
-            input_data = await get_features(transaction.to)
+            signed_tx_bytes = eth_utils.to_bytes(hexstr=rpc.params[0])
+            decoded_tx = TransactionBuilder().decode(signed_tx_bytes)
+            sender = eth_utils.encode_hex(decoded_tx.sender)
+            to = eth_utils.to_hex(decoded_tx.to)
+            input_data = await get_features(to)
             score, proof = await check_fraud(input_data)
             if score > 0.5:
                 return {
@@ -162,13 +209,13 @@ async def root(rpc: RpcRequest):
                     'id': rpc.id
                 }
             else:
-                contract_address = await get_contract_wallet_address(transaction.from_)
+                contract_address = await get_contract_wallet_address(sender)
                 wallet_contract = w3.eth.contract(
                     address=contract_address, abi=contract_abi)
                 tx = wallet_contract.functions.execute(
-                    transaction.serialize(), input_data, proof).build_transaction(
+                    decoded_tx.data, input_data, proof).build_transaction(
                         {
-                            'nonce': transaction.nonce,
+                            'nonce': decoded_tx.nonce,
                             'from': account.address,
                             'to': contract_address
                         }
@@ -179,7 +226,7 @@ async def root(rpc: RpcRequest):
                     'result': tx_hash,
                     'id': rpc.id
                 }
-    print('Relaying rpc message %s to infura' % str(rpc))
+    print('Relaying rpc %s to infura' % str(rpc.method))
     async with httpx.AsyncClient() as client:
         response = await client.post(
             INFURA_URL,
